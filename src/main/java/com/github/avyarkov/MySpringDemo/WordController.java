5f2aeb43d21e64b5bf2a5779f4a63b37fe052873
avyarkov <jarkov.alexej@gmail.com>
2023-01-23 01:43:17 +0300

    putting things in order

--- src/main/java/com/github/avyarkov/MySpringDemo/WordController.java
+++ src/main/java/com/github/avyarkov/MySpringDemo/WordController.java
@@ -4,12 +4,13 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping
 public class WordController {
     private final WordService wordService;
@@ -21,20 +22,15 @@ public class WordController {
         this.objectMapper = new ObjectMapper();
     }
 
-    @GetMapping(path = "/")
+    @GetMapping(path = {"/", "/home"})
     public ModelAndView home() {
         return new ModelAndView("home");
     }
 
     @GetMapping(path = "/words", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
     public String getWordsWithWordCounts() throws JsonProcessingException {
         return objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(wordService.getWordToWordcountMap());
-    }
-
-    @GetMapping(path = "/addWord")
-    public ModelAndView addWordGetWithParam(@RequestParam String word) {
-        wordService.addWord(word);
-        return new ModelAndView("home");
     }
 
     @GetMapping(path = "/add")
@@ -42,7 +38,15 @@ public class WordController {
         return new ModelAndView("addWordForm");
     }
 
-    @PostMapping(path = "/add")
+    // "hidden" auxiliary endpoints for testing things out
+
+    @GetMapping(path = "/addWord")
+    public ModelAndView addWordGetWithParam(@RequestParam String word) {
+        wordService.addWord(word);
+        return new ModelAndView("home");
+    }
+
+    @PostMapping(path = "/addWord")
     public ModelAndView addWordPost(@RequestParam(name = "word") String word) {
         wordService.addWord(word);
         return new ModelAndView("home");
